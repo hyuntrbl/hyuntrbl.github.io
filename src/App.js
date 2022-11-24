84c7c330dea9c2ac9017217b37e239f3f6d88b2c
Hyunwoo Cho <truetype24@gmail.com>
2022-11-24 17:14:33 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <div className='Container'>
+        <div className="Container">
+          <a className="Big" href="https://www.behance.net/gallery/156250441/RIDM-Rare-Incurable-Disease-Management" target="_blank" rel="noreferrer">
+            221022_RIDM
+          </a>
+          <a className="Big" href="https://github.com/IngoteamSunrin/Ingopass_Backend" target="_blank" rel="noreferrer">
+            220918_Ingopass
+          </a>
+          <a className="Big" href="https://www.behance.net/gallery/138293955/Flow-Music-Streaming" target="_blank" rel="noreferrer">
+            220227_Flow
+          </a>
+          <a className="Big" href="https://www.behance.net/gallery/129003675/Rhime" target="_blank" rel="noreferrer">
+            211010_Rhime
+          </a>
+        </div>
+        <div className='Container'>
+        <a className="App-link" href="https://behance.net/hyunwoocho" target="_blank" rel="noreferrer">
+          @hyunwoocho
         </a>
-      </header>
+        <a className="App-link" href="https://instagram.com/d3signedby" target="_blank" rel="noopener noreferrer">
+          @d3signedby
+        </a>
+        <a className="App-link" href="https://github.com/hyuntrbl" target="_blank" rel="noopener noreferrer">
+          @hyuntrbl
+        </a>
+        </div>
+      </div>
     </div>
   );
 }
